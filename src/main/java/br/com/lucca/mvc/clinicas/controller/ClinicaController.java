b573fd821b3f65bec86af382808feac511ade25f
Lucca Brito <luccabritogt@gmail.com>
2022-04-05 18:56:03 -0300

    ajustado o ClinicaController para que os métodos utilizem o DTO e façam validação @NotBlank em alguns campos do formulário

--- src/main/java/br/com/lucca/mvc/clinicas/controller/ClinicaController.java
+++ src/main/java/br/com/lucca/mvc/clinicas/controller/ClinicaController.java
@@ -1,15 +1,37 @@
 package br.com.lucca.mvc.clinicas.controller;
 
+import br.com.lucca.mvc.clinicas.dto.RequisicaoNovaClinica;
+import br.com.lucca.mvc.clinicas.model.Clinica;
+import br.com.lucca.mvc.clinicas.repository.ClinicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("clinica")
 public class ClinicaController {
 
+    @Autowired
+    private ClinicaRepository clinicaRepository;
+
     @GetMapping("formulario")
-    public String formulario() {
+    public String formulario(RequisicaoNovaClinica requisicao) {
         return "clinica/formulario";
+    }
+
+    @PostMapping("novo")
+    public String novo(@Valid RequisicaoNovaClinica requisicao, BindingResult result) {
+        if (result.hasErrors()) {
+            return "clinica/formulario";
+        } else {
+            Clinica clinica = requisicao.toClinica();
+            clinicaRepository.save(clinica);
+            return "home";
+        }
     }
 }

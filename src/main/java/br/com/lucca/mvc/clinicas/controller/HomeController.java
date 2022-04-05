f477fb037e2bf4044c1c8ccf748dac24aa89b0fd
Lucca Brito <luccabritogt@gmail.com>
2022-04-05 12:02:14 -0300

    retirada a responsabilidade de conexão com a database do controller -> tarefa relegada para o repository

--- src/main/java/br/com/lucca/mvc/clinicas/controller/HomeController.java
+++ src/main/java/br/com/lucca/mvc/clinicas/controller/HomeController.java
@@ -1,28 +1,24 @@
 package br.com.lucca.mvc.clinicas.controller;
 
 import br.com.lucca.mvc.clinicas.model.Clinica;
+import br.com.lucca.mvc.clinicas.repository.ClinicaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    private ClinicaRepository repository;
 
     @GetMapping("/home")
     public String home(Model model) {
-
-        Query query = entityManager.createQuery("select p from Clinica p", Clinica.class);
-        List<Clinica> clinicas = query.getResultList();
-
+        List<Clinica> clinicas = repository.recuperaTodasAsClinicas();
         model.addAttribute("clinicas", clinicas);
         return "home";
     }

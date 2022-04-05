46c028cefd1ece1bbdd6183d00312f8a9f83d43d
Lucca Brito <luccabritogt@gmail.com>
2022-04-05 11:28:27 -0300

    adicionada a notação @Entity no modelo e criação do atributo ID

--- src/main/java/br/com/lucca/mvc/clinicas/model/Clinica.java
+++ src/main/java/br/com/lucca/mvc/clinicas/model/Clinica.java
@@ -1,6 +1,15 @@
 package br.com.lucca.mvc.clinicas.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Clinica {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nomeClinica;
     private String endereco;

142472d2bdc40527edb76264143ce5e184d562e3
Lucca Brito <luccabritogt@gmail.com>
2022-04-05 10:55:21 -0300

    criado o modelo da clinica

---
+++ src/main/java/br/com/lucca/mvc/clinicas/model/Clinica.java
@@ -0,0 +1,50 @@
+package br.com.lucca.mvc.clinicas.model;
+
+public class Clinica {
+
+    private String nomeClinica;
+    private String endereco;
+    private String telefone;
+    private String abordagem;
+    private String descricao;
+
+    public String getNomeClinica() {
+        return nomeClinica;
+    }
+
+    public void setNomeClinica(String nomeClinica) {
+        this.nomeClinica = nomeClinica;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getAbordagem() {
+        return abordagem;
+    }
+
+    public void setAbordagem(String abordagem) {
+        this.abordagem = abordagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}

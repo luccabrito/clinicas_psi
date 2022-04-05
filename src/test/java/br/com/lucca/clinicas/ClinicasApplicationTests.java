146c9686133f7d4dc5b7ef29a254ac821d4cdb2f
Lucca Brito <luccabritogt@gmail.com>
2022-04-05 10:47:34 -0300

    commit inicial - projeto spring boot com maven e dependências instaladas

---
+++ src/test/java/br/com/lucca/clinicas/ClinicasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucca.clinicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

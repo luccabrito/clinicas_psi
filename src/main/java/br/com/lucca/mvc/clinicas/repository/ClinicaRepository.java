4624c942c502d8f6e16bebe0a4f995569caec815
Lucca Brito <luccabritogt@gmail.com>
2022-04-05 12:07:47 -0300

    adaptando o repository para usar Spring Data JPA (ao invés de classe, interface que extende JpaRepository)

--- src/main/java/br/com/lucca/mvc/clinicas/repository/ClinicaRepository.java
+++ src/main/java/br/com/lucca/mvc/clinicas/repository/ClinicaRepository.java
@@ -1,21 +1,12 @@
 package br.com.lucca.mvc.clinicas.repository;
 
 import br.com.lucca.mvc.clinicas.model.Clinica;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import java.util.List;
 
 @Repository
-public class ClinicaRepository {
+public interface ClinicaRepository extends JpaRepository<Clinica, Long> {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public List<Clinica> recuperaTodasAsClinicas() {
-        Query query = entityManager.createQuery("select p from Clinica p", Clinica.class);
-        return query.getResultList();
-    }
 }

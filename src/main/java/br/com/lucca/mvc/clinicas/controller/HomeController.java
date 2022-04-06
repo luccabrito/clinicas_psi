6af2112a6f09b699f1e6382bacc34cd237b3eb24
Lucca Brito <luccabritogt@gmail.com>
2022-04-06 16:44:55 -0300

    criado o método que responde à requisicao /home/emergencia

--- src/main/java/br/com/lucca/mvc/clinicas/controller/HomeController.java
+++ src/main/java/br/com/lucca/mvc/clinicas/controller/HomeController.java
@@ -6,20 +6,39 @@ import br.com.lucca.mvc.clinicas.repository.ClinicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping(value = "/home")
 public class HomeController {
 
     @Autowired
     private ClinicaRepository pedidoRepository;
 
-    @GetMapping("/home")
+    @GetMapping
     public String home(Model model) {
         List<Clinica> clinicas = pedidoRepository.findAll();
         model.addAttribute("clinicas", clinicas);
+        return "home";
+    }
+
+    @GetMapping("/emergencia")
+    public String emergencia(Model model) {
+        List<Clinica> clinicas = pedidoRepository.findAll();
+        List<Clinica> clinicasComAtendimentoEmergencial = new ArrayList<>();
+        for (Clinica clinica : clinicas) {
+            if (clinica.temAtendimentoEmergencial()) {
+                clinicasComAtendimentoEmergencial.add(clinica);
+            }
+        }
+        Boolean verdade = true;
+        model.addAttribute("clinicas", clinicasComAtendimentoEmergencial);
+        model.addAttribute("emergencia", verdade);
         return "home";
     }
 }
